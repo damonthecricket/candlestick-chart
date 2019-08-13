046f7bc5534860bcd9d833de49ade12648d333df
Damon Cricket <d.a.tsvirko@gmail.com>
2019-08-13 20:09:06 +0200

    Final commit.

--- install_libs.sh
+++ install_libs.sh
@@ -3,11 +3,16 @@ mkdir libs
 
 
 git clone https://github.com/damonthecricket/pyutils libs/pyutils
+git pull https://github.com/damonthecricket/pyutils
+
+git clone https://github.com/damonthecricket/pystock libs/pystock
+git pull https://github.com/damonthecricket/pystock
 
 
 cat <<EOF > libs/__init__.py
 
 __all__ = [
-	"pyutils"
+	'pystock',
+	'pyutils'
 ]
 EOF
--- unit_tests/main.py
+++ unit_tests/main.py
@@ -2,8 +2,6 @@
 import unittest
 
 from . import test_axe
-from . import test_db
-from . import test_model
 from . import test_shape
 
 
@@ -11,9 +9,7 @@ loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 suite.addTests( loader.loadTestsFromModule(test_axe)   )
-suite.addTests( loader.loadTestsFromModule(test_db)    )
-suite.addTests( loader.loadTestsFromModule(test_model) )
 suite.addTests( loader.loadTestsFromModule(test_shape) )
 
-runner = unittest.TextTestRunner(verbosity = 3)
+runner = unittest.TextTestRunner(verbosity = 2)
 result = runner.run(suite)
